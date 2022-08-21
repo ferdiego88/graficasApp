22f84b996e82c1720770d67c1689219d5c338114
Fernando Santillan <ferdiego88@gmail.com>
2022-08-20 21:55:02 -0500

    fsv dona

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
@@ -8,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaHttpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
+    this.graficasService.getDataSocialMedia()
+      .subscribe(data => {
+        this.doughnutChartData = {
+          labels: Object.keys(data),
+          datasets: [{data: Object.values(data)}]
+        }
+
+      });
   }
+
+   // Doughnut
+   public doughnutChartLabels: string[] = [
+    //'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Computer Sales'
+  ];
+   public doughnutChartData: ChartData<'doughnut'> = {
+     labels: this.doughnutChartLabels,
+     datasets: [
+       { data: [
+        //350, 450, 100, 140
+       ],
+         backgroundColor: ['#FAC374','#6f2fd3','#ba00ad','#FA1157'],
+         hoverBackgroundColor: ['#FAC374','#6f2fd3','#ba00ad','#FA1157'],
+         hoverBorderColor:['#FAC374','#6f2fd3','#ba00ad','#FA1157'], }
+     ]
+   };
+   public doughnutChartType: ChartType = 'doughnut';
 
 }
--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -12,5 +13,18 @@ export class DonaComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+    // Doughnut
+    public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Computer Sales' ];
+    public doughnutChartData: ChartData<'doughnut'> = {
+      labels: this.doughnutChartLabels,
+      datasets: [
+        { data: [ 350, 450, 100, 140 ],
+          backgroundColor: ['#FAC374','#6f2fd3','#ba00ad','#FA1157'],
+          hoverBackgroundColor: ['#FAC374','#6f2fd3','#ba00ad','#FA1157'],
+          hoverBorderColor:['#FAC374','#6f2fd3','#ba00ad','#FA1157'], }
+      ]
+    };
+    public doughnutChartType: ChartType = 'doughnut';
 
 }
